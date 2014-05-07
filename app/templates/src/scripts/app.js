0d21220769c730e5e90055ef142f6208190ab8a6
Adam Thody <adam@adamthody.com>
2014-05-07 19:15:17 -0400

    Add JS scaffold

---
+++ app/templates/src/scripts/app.js
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+"use strict";
+
+var React = require('react');
+
+var App = React.createClass({
+    render: function () {
+            
+    }
+});
+
+React.renderComponent(<App />, document.getElementById('app-container'));
